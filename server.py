173702b494d7e2be08257225946e0ae1998c70c4
Maximo Hinojosa <maximohinojosa13@gmail.com>
2021-01-28 02:22:41 -0800

    [Update] - created base logic for server

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import json
+from flask_restful import Api
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = "mongodb://localhost:27017/levelupDB"
+mongo = PyMongo(app) # initializing db
+api = Api(app) # initializing API
+
+
+
+@app.route('/', methods=["GET"])
+def get():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
