f7d7e0a45bf290a3614d735aa56a38d3898e015a
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2021-01-29 22:16:22 -0700

    [Update] - implemented base logic for post method

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_pymongo import PyMongo
 
@@ -9,10 +8,28 @@ mongo = PyMongo(app) # initializing db
 api = Api(app) # initializing API
 
 
+class Task():
+    """Task Model"""
+    
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+    def convert_to_json(self):
+        """converts objects into json"""
+        return self.__dict__
+
+
 
 @app.route('/', methods=["GET"])
 def get():
-    return "Hello World"
+    return "Hello pineapple"
+
+@app.route('/task', methods=["POST"])
+def post():
+    data = request.get_json()
+    return jsonify(data)
 
 
 if __name__ == '__main__':
